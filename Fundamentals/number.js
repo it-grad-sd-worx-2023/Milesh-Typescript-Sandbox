8d91c41834dfbc67a94f5e6eefd11891139331b9
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-07 10:00:21 +0400

    number fundamentals

---
+++ Fundamentals/number.js
@@ -0,0 +1,12 @@
+var price = 5;
+var newPrice = 9.95;
+var bin = 4;
+var octal = 8;
+var hex = 0xA;
+var bigInt = 5331610;
+console.log(price);
+console.log(newPrice);
+console.log(bin);
+console.log(octal);
+console.log(hex);
+console.log(bigInt);
---
+++ Fundamentals/number.ts
@@ -0,0 +1,13 @@
+let price: number = 5
+let newPrice = 9.95;
+let bin = 0b100; 
+let octal = 0o10;
+let hex: number = 0xA;
+let bigInt = 0o24255232;
+
+console.log(price); 
+console.log(newPrice); 
+console.log(bin); 
+console.log(octal); 
+console.log(hex); 
+console.log(bigInt); 
