ec711c3921125af8d91a5d3b26a68f814d438cc6
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-13 13:56:48 +0400

    Spread-Operator For object Fundamentals

--- Fundamentals/spread-operator.js
+++ Fundamentals/spread-operator.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
@@ -11,4 +22,11 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 var originalArray = [1, 2, 3];
 var newArray = __spreadArray(__spreadArray([], originalArray, true), [4, 5], false);
 console.log(newArray);
-console.log(originalArray);
+// console.log(originalArray);
+// spread operator for object
+var originalObject = { name: 'Milesh', age: 25, city: 'Port Louis' };
+var copyObject = __assign({}, originalObject);
+// console.log(copyObject);
+//modify the object 
+copyObject.age = 50;
+console.log(copyObject);
--- Fundamentals/spread-operator.ts
+++ Fundamentals/spread-operator.ts
@@ -2,4 +2,18 @@
 const originalArray = [1,2,3];
 const newArray = [...originalArray,4,5];
 console.log(newArray);
-console.log(originalArray);
+// console.log(originalArray);
+
+
+
+// spread operator for object
+const originalObject = { name : 'Milesh', age: 25, city: 'Port Louis'};
+
+const copyObject = {...originalObject};
+// console.log(copyObject);
+
+
+//modify the object 
+
+copyObject.age = 50;
+console.log(copyObject)
