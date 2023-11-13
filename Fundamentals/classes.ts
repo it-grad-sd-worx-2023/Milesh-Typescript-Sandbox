cdbc962f7f95161543f2c9b9bc009ba6c34af8a3
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-13 16:39:52 +0400

    classes

--- Fundamentals/classes.ts
+++ Fundamentals/classes.ts
@@ -1,20 +1,20 @@
 class Person { 
-    ssn : string;
-    first_name : string;
-    last_name : string;
+    // public  ssn : string;
+    //  private first_name : string;
+    // protected last_name : string;
 
 
     //creating a custructor 
-    constructor(ssn: string, first_name : string , last_name :string ){
+    constructor( public ssn: string, private first_name : string , private last_name :string ){
         this.ssn = ssn;
         this.first_name = first_name;
         this.last_name = last_name;
     }
-
-
-    
     getFullName(): string {
         return `${this.first_name} ${this.last_name}`
     }
-
 }
+
+
+let secondPerson = new Person("1023", 'John', 'Do')
+console.log(secondPerson.ssn)
