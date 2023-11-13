b9bfee8826028e4584b3535f18e31428329935c9
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-13 11:40:13 +0400

    type any

---
+++ Fundamentals/any.js
@@ -0,0 +1,9 @@
+var json = "{\"latitude\": 10.1240, \"longitude\": 50.71}";
+var currentLocation = JSON.parse(json);
+// this is console logging out the currentLocation
+// this mean the longtitude and latitude of json
+console.log(currentLocation);
+// here we are console loggin the currentLocation latitude
+console.log(currentLocation.latitude);
+//this will give us 
+console.log(currentLocation.x);
---
+++ Fundamentals/any.ts
@@ -0,0 +1,20 @@
+const json = `{"latitude": 10.1240, "longitude": 50.71}`;
+const currentLocation = JSON.parse(json);
+
+// this is console logging out the currentLocation
+// this mean the longtitude and latitude of json
+console.log(currentLocation);
+
+
+
+// here we are console loggin the currentLocation latitude
+
+console.log(currentLocation.latitude);
+
+
+//this will give us why because x doesnt exist in currentLocation
+// when you get undefined error is because the data you are trying to check or fetch is not present in the code .
+
+console.log(currentLocation.x);
+
+
