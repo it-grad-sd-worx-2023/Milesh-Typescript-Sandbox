76f349fd45d858f3ea0ba12484d7a29ff7d54bc4
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-14 09:17:39 +0400

    homework1 days of the week

---
+++ Fundamentals/daysOfWeek.ts
@@ -0,0 +1,9 @@
+// Define an array of days of the week
+const daysOfWeek: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+// Print the contents of the array to the console
+console.log("Days of the Week:");
+daysOfWeek.forEach(day => {
+  console.log(day);
+  // Comment: No apparent mistakes in this section, looks good!
+});
