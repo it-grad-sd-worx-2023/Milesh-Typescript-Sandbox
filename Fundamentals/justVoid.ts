7c78a8ca78b84b9df8e4c1991bb4cbfc0d6c47ec
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-15 16:22:38 +0400

    Another void session

---
+++ Fundamentals/justVoid.ts
@@ -0,0 +1,37 @@
+// Function to add two numbers
+function add(a: number, b: number): void {
+    console.log(`Result of ${a} + ${b} is ${a + b}`);
+}
+
+// Function to subtract two numbers
+function subtract(a: number, b: number): void {
+    console.log(`Result of ${a} - ${b} is ${a - b}`);
+}
+
+// Function to multiply two numbers
+function multiply(a: number, b: number): void {
+    console.log(`Result of ${a} * ${b} is ${a * b}`);
+}
+
+// Function to divide two numbers
+function divide(a: number, b: number): void {
+    if (b !== 0) {
+        console.log(`Result of ${a} / ${b} is ${a / b}`);
+    } else {
+        console.error("Cannot divide by zero!");
+    }
+}
+
+// Call the calculator functions
+add(5, 3);
+subtract(8, 4);
+multiply(2, 6);
+divide(10, 2);
+divide(8, 0); // Attempt to divide by zero
+
+// Output:
+// Result of 5 + 3 is 8
+// Result of 8 - 4 is 4
+// Result of 2 * 6 is 12
+// Result of 10 / 2 is 5
+// Cannot divide by zero!
