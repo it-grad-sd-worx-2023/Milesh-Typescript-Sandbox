44420cf1cf1771932e848e78b582fe47f87720cc
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-15 16:06:52 +0400

    string fundamentals

---
+++ Fundamentals/strings.js
@@ -0,0 +1,4 @@
+var first_name = "Milesh";
+var description = "This TypeScript string can\nspan multiple\nlines!";
+var profile = "I am ".concat(first_name, ".");
+console.log(profile);
---
+++ Fundamentals/strings.ts
@@ -0,0 +1,8 @@
+let first_name: string = "Milesh";
+let description = `This TypeScript string can
+span multiple
+lines!`;
+
+let profile: string = `I am ${first_name}.`;
+
+console.log(profile);
