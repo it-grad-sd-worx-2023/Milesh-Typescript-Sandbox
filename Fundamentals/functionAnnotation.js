c8beefd768ba96f596c2004a184c47bd9de0d216
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-20 10:23:02 +0400

    functionAnnotation Fundamentals

---
+++ Fundamentals/functionAnnotation.js
@@ -0,0 +1,5 @@
+function add(x, y) {
+    return x + y;
+}
+var result = add(3, 5);
+console.log("Result: ".concat(result));
---
+++ Fundamentals/functionAnnotation.ts
@@ -0,0 +1,7 @@
+function add(x: number, y: number): number {
+    return x + y;
+  }
+  
+  let result: number = add(3, 5);
+  console.log(`Result: ${result}`);
+  
