79f21c715a9264ca007d5ca4f1edc60dcb5c6276
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-20 09:43:01 +0400

    Members Types

---
+++ Fundamentals/memberTypes.js
@@ -0,0 +1,11 @@
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var person = new Person();
+person.name = "hello";
+person.age = 10;
+person.city = true;
+person.address = "Port Louis";
+console.log(person);
---
+++ Fundamentals/memberTypes.ts
@@ -0,0 +1,13 @@
+class Person {
+    name:string;
+    age: number;
+    city: boolean;
+    address: string;
+}
+
+const person = new Person();
+person.name = "hello"
+person.age = 10;
+person.city = true;
+person.address = "Port Louis";
+console.log(person);
