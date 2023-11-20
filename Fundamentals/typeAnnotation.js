0a2dff7c36937d20ecda995df9a9e7efd86b8827
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-20 10:21:40 +0400

    typeAnnotation.ts

---
+++ Fundamentals/typeAnnotation.js
@@ -0,0 +1,6 @@
+var message = "Hello, TypeScript!";
+var count = 42;
+var isDone = false;
+var numbers = [1, 2, 3, 4, 5];
+var names = ["Alice", "Bob", "Charlie"];
+console.log(numbers);
---
+++ Fundamentals/typeAnnotation.ts
@@ -0,0 +1,7 @@
+let message: string = "Hello, TypeScript!";
+let count: number = 42;
+let isDone: boolean = false;
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+let names: string[] = ["Alice", "Bob", "Charlie"];
+console.log(numbers);
