b6e46259b417297cec416d838e9894238e4a7c79
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-22 17:14:02 +0400

    commented out code for fix file stucture

---
+++ Homework 4/fix.ts
@@ -0,0 +1,21 @@
+// export type User = unknown;
+
+// export const users: object[] = [
+//    {
+//         name: 'Max Mustermann',
+//         age: 25,
+//         occupation: 'Chimney sweep'
+//    },
+//    {
+//         name: 'Kate Müller',
+//         age: 23,
+//         occupation: 'Astronaut'
+//    }
+// ];
+
+// export function logPerson(user: object) {
+//    console.log(` - ${user.name}, ${user.age}`);
+// }
+
+// console.log('Users:');
+// users.forEach(logPerson);
