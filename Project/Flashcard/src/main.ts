6ae93c1083072df9f888d1c68cb7c2802ff9e239
SD580683 <milesh.taukoorah@sdworx.com>
2023-11-27 14:33:53 +0400

    added  main and flashcard now we are going to flip the cards accordingly using the p tag and then flip the card when click to trigger the flip

---
+++ Project/Flashcard/src/main.ts
@@ -0,0 +1,42 @@
+import { FlashCard } from "./flashcard.js";
+ 
+const flashCards: FlashCard[] = [
+    new FlashCard('Javascript', 'A popular programming language used for web development.'),
+    new FlashCard('Typescript', 'A statically typed superset of javascript.')
+];
+ 
+function showFlashCards(){
+    const title = document.getElementById('title');
+    const content = document.getElementById('content');
+    content.innerHTML="";
+    title.innerHTML = '<h2>Flash cards</h2>';
+
+    for (const flashcard of flashCards){
+        const cardDiv = document.createElement('div');
+        const cardBodyDiv = document.createElement('div');
+        cardDiv.className = 'card mb-3 bg-secondary text-white me-2';
+        cardBodyDiv.className = 'card-body';
+        cardDiv.appendChild(cardBodyDiv);
+        cardBodyDiv.innerHTML = `
+        <p class = "term">
+            <span class = "fw-bold">Term: </span> ${flashcard.term}
+        </p>    
+
+
+        <p class = "definition d-none">
+            <span class = "fw-bold">Definition: </span> ${flashcard.definition}
+        </p>    
+
+       `
+       content.appendChild(cardDiv);
+    }
+}
+
+function flipCard(): void {
+
+} 
+
+
+
+flipCard();
+showFlashCards();
